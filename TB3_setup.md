ee086645dc60b988805b367fca3f7087a3e87f0f
nabeya11 <nabeya11th.sanza.electric.book@gmail.com>
2020-10-13 10:58:59 +0900

    #update subscribeできないときは0を送る

--- TB3_setup.md
+++ TB3_setup.md
@@ -137,3 +137,5 @@ PC
 ```bash
 $ roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch
 ```
+WASDXで動けばOK
+turtlebotの「前」はタイヤ(駆動輪)がある方
--- multi_navigation/src/vel_publisher.py
+++ multi_navigation/src/vel_publisher.py
@@ -6,28 +6,53 @@ import rospy
 import numpy
 from geometry_msgs.msg import Twist
 
-global twist_avoidance
-twist_avoidance=Twist()
+global twist_avoid
+global twist_comp
+global cnt
+twist_avoid=Twist()
+twist_comp=Twist()
 
 def destination_subscriber():
     rospy.init_node('comp_vel_publisher',anonymous=True)
     rospy.Subscriber('destination_component', Twist, vel_publisher)
     rospy.Subscriber('avoidance_component', Twist, avoidcallback)
-    rospy.spin()
+    compvel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(10.0)
+    zerovel = Twist()
+    zerovel.linear.x = 0.0
+    zerovel.angular.z = 0.0
+
+    global cnt
+    cnt=0
+
+    global twist_avoid
+    global twist_comp
+
+
+    while not rospy.is_shutdown():
+        if(cnt>6):
+            compvel_publisher.publish(zerovel)
+        else:
+            if numpy.sign(twist_avoid.linear.x) != numpy.sign(twist_comp.linear.x):
+                twist_comp.linear.x *= 1.0 - abs(twist_avoid.linear.x)
+                twist_comp.angular.z += twist_avoid.angular.z
+            # print twist_avoid
+            # print(twist_avoid.linear.x)
+            compvel_publisher.publish(twist_comp)
+
+            cnt = cnt + 1
+
+        rate.sleep()
 
 def avoidcallback(avoid):
-    global twist_avoidance
-    twist_avoidance = avoid
+    global twist_avoid
+    twist_avoid = avoid
 
-def vel_publisher(comp):
-    global twist_avoidance
-    compvel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    if numpy.sign(twist_avoidance.linear.x) != numpy.sign(comp.linear.x):
-        comp.linear.x *= 1.0 - abs(twist_avoidance.linear.x)
-        comp.angular.z += twist_avoidance.angular.z
-    print twist_avoidance
-    # print(twist_avoidance.linear.x)
-    compvel_publisher.publish(comp)
+def vel_publisher(dest):
+    global cnt
+    global twist_comp
+    twist_comp = dest
+    cnt = 0
 
 if __name__ == '__main__':
     try:
