a242d76aca8a1fd92e18a8e9a532c9a78d48120d
nabeya11 <nabeya11th.sanza.electric.book@gmail.com>
2020-10-14 17:56:35 +0900

    #update judge enable in set_initpos

--- multi_navigation/src/set_initpos.py
+++ multi_navigation/src/set_initpos.py
@@ -11,19 +11,20 @@ if __name__ == '__main__':
 
     print("follow are the names of robot which is set initpos")
     for robot_info in robot_list:
-      print(robot_info['name'])
-      pub = rospy.Publisher(robot_info['name']+'/initialpose', PoseWithCovarianceStamped, queue_size=0, latch=True)
-      initpos = PoseWithCovarianceStamped()
+      if not target['enable']:
+        print(robot_info['name'])
+        pub = rospy.Publisher(robot_info['name']+'/initialpose', PoseWithCovarianceStamped, queue_size=0, latch=True)
+        initpos = PoseWithCovarianceStamped()
 
-      initpos.header.stamp = rospy.Time.now()
-      initpos.header.frame_id = 'map'
-      initpos.pose.pose.position.x = robot_info['init_pos'][0]
-      initpos.pose.pose.position.y = robot_info['init_pos'][1]
-      initpos.pose.pose.orientation.z = robot_info['init_pos'][2]
-      initpos.pose.pose.orientation.w = 1
-      # initpos.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+        initpos.header.stamp = rospy.Time.now()
+        initpos.header.frame_id = 'map'
+        initpos.pose.pose.position.x = robot_info['init_pos'][0]
+        initpos.pose.pose.position.y = robot_info['init_pos'][1]
+        initpos.pose.pose.orientation.z = robot_info['init_pos'][2]
+        initpos.pose.pose.orientation.w = 1
+        # initpos.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
 
-      pub.publish(initpos)
+        pub.publish(initpos)
 
     print("Finished settting all initial position")
     rospy.spin()
