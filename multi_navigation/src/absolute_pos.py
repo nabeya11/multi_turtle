#fix some quaternion calculation

multi_navigation/src/absolute_pos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy
+import tf
+import tf2_ros
 from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
@@ -9,24 +11,28 @@ if __name__ == '__main__':
     r = rospy.Rate(60)
     listener = tf.TransformListener()
     abs_pos = Pose()
+    pub = list()
 
     robot_list = rospy.get_param('robot_list')
 
     for robot_info in robot_list:
       if robot_info['enable']:
-        pub[robot_info['id']] = rospy.Publisher(robot_info['name']+'/absolute_pos', Pose, queue_size=0, latch=True)
+        pub.append(rospy.Publisher(robot_info['name']+'/absolute_pos', Pose, queue_size=0, latch=True))
+      else:
+        pub.append(0)
 
     while not rospy.is_shutdown():
       for robot_info in robot_list:
         if robot_info['enable']:
           try:
             listener.waitForTransform("/map", robot_info['name'] + "/base_footprint", rospy.Time(0), rospy.Duration(10.0))
-            (trans,rot) = listener.lookupTransform(self.my_name + "/base_footprint", target_name + "/base_footprint", tfnow)
+            (trans,rot) = listener.lookupTransform("/map", robot_info['name'] + "/base_footprint", rospy.Time(0))
             abs_pos.position.x = trans[0]
             abs_pos.position.y = trans[1]
-            abs_pos.orientation.z = rot[2]
-
-            return pos
+            e = tf.transformations.euler_from_quaternion(rot)
+            abs_pos.orientation.z = e[2]
+            print(robot_info['name'])
+            print(abs_pos.orientation.z)
 
           except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, tf2_ros.TransformException) as error:
             print(error)

multi_navigation/src/set_initpos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy
+import tf
+import math
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 if __name__ == '__main__':
@@ -20,9 +22,16 @@ if __name__ == '__main__':
         initpos.header.frame_id = 'map'
         initpos.pose.pose.position.x = robot_info['init_pos'][0]
         initpos.pose.pose.position.y = robot_info['init_pos'][1]
-        initpos.pose.pose.orientation.z = robot_info['init_pos'][2]
-        initpos.pose.pose.orientation.w = 1
-        # initpos.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+        initpos.pose.pose.orientation.w = 1.0
+        initquat = tf.transformations.quaternion_from_euler(0, 0, robot_info['init_pos'][2])
+        initpos.pose.pose.orientation.x = initquat[0]
+        initpos.pose.pose.orientation.y = initquat[1]
+        initpos.pose.pose.orientation.z = initquat[2]
+        initpos.pose.pose.orientation.w = initquat[3]
+        print(initpos.pose.pose.orientation)
+        initpos.pose.covariance[6*0+0] = 0.5 * 0.5
+        initpos.pose.covariance[6*1+1] = 0.5 * 0.5
+        initpos.pose.covariance[6*5+5] = math.pi/12.0 * math.pi/12.0
 
         pub.publish(initpos)
 